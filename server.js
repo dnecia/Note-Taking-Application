47320b582b2551e29927a4eef46cc3c55b33063a
Donniecia  Corke <donnieciamc@gmail.com>
2022-11-01 15:50:03 -0400

    troubleshooting the page to load on start

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
