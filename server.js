829c57cd5015a19468b1d118e419170c3c5b39ec
Donniecia  Corke <donnieciamc@gmail.com>
2022-11-01 15:18:17 -0400

    adding the get and use in the server.js file

---
+++ server.js
@@ -0,0 +1,38 @@
+const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+const allNotes = require('./db/db.json');
+
+app.use(express.urlencoded ({ extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api/notes', (req,res)=>{
+    res.json(allNotes.slice(1));
+});
+
+app.get('/', (req,res)=> {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+    notesArray = [];
+    if (notesArray.length === 0)
+    notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+    
+
+
+}
